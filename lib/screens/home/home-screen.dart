4fdaddf1ecf45bc8c257e03df42ada2d47782e07
EduardRo <eduardstancu@yahoo.com>
2021-08-11 13:06:36 +0300

    creation of files

---
+++ lib/screens/home/home-screen.dart
@@ -0,0 +1,12 @@
+import 'package:flt_cab_1108/constants.dart';
+import 'package:flt_cab_1108/models/Product.dart';
+import 'package:flutter/material.dart';
+import 'components/header.dart';
+import 'components/product_card.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
